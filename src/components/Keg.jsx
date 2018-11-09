83c4366376027758d58c41241ffaa7f472c5e33f
Kendra <kendra@coffeeringsdesign.com>
2018-11-09 13:10:01 -0800

    get the full imformation and picture of kegs to show

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -2,17 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Keg(props) {
-  const kegStyles ={
+  const kegStyles = {
+    display: 'flex',
     marginLeft: 'auto',
     marginRight: 'auto',
     marginTop: '50px',
-    width: '80%',
-    backgroundColor: 'red'
+    width: '60%',
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    textAlign: 'left'
+  }
+  const kegInformationStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'space-around'
   }
 
   return(
-    <div>
-      <h3 style={kegStyles}>{props.img}</h3>
+    <div style={kegStyles}>
+      <div>
+        <img src={props.img}/>
+      </div>
+      <div>
+        <div style={kegInformationStyles}>
+          <h1>{props.name}</h1>
+          <h2>{props.brewery}</h2>
+          <h3>{props.style}</h3>
+          <h4>ABV: {props.abv}</h4>
+          <h4>IBU: {props.ibu}</h4>
+          <h4>Price: {props.price}</h4>
+          <h4>Pints Left in Keg: {props.pintCount}</h4>
+          <h4>Region: {props.region}</h4>
+        </div>
+      </div>
     </div>
   );
 }
