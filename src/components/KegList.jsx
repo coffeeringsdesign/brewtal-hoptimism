ffb4abe9fab10d99c2703586b0ad00a844c93345
Kendra <kendra@coffeeringsdesign.com>
2018-11-16 09:31:18 -0800

    make KegList stateful, move css, move hardcoded beers into state

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -10,158 +10,179 @@ import citradelic from '../assets/images/citradelic.jpg';
 import furious from '../assets/images/furious.jpg';
 import sculpin from '../assets/images/sculpin.jpg';
 
-const masterTappedKegList = [
-  {
-    tapped: true,
-    name: 'RPM',
-    brewery: 'Boneyard',
-    img: rpm,
-    style: 'IPA',
-    abv: 6.5,
-    ibu: '50',
-    price: '$7',
-    pintCount: 101,
-    region: 'local'
-  },
-  {
-    tapped: true,
-    name: 'Sculpin',
-    brewery: 'Ballast Point',
-    img: sculpin,
-    style: 'IPA',
-    abv: 7,
-    ibu: '70',
-    price: '$10',
-    pintCount: 23,
-    region: 'national'
-  },
-  {
-    tapped: true,
-    name: 'Furious',
-    brewery: 'Surly Brewing Co',
-    img: furious,
-    style: 'IPA',
-    abv: 6.7,
-    ibu: 'high',
-    price: '$9',
-    pintCount: 64,
-    region: 'national'
-  },
-  {
-    tapped: true,
-    name: 'Super Dog',
-    brewery: 'Lucky Labrador',
-    img: luckyLab,
-    style: 'IPA',
-    abv: 6.5,
-    ibu: '90+',
-    price: '$8',
-    pintCount: 55,
-    region: 'local'
-  },
-  {
-    tapped: true,
-    name: 'Citradelic',
-    brewery: 'New Belgium',
-    img: citradelic,
-    style: 'Tangerine IPA',
-    abv: 6,
-    ibu: '50',
-    price: '$7',
-    pintCount: 1002,
-    region: 'national'
-  },
-  {
-    tapped: true,
-    name: 'Hop Venom',
-    brewery: 'Boneyard',
-    img: hopVenom,
-    style: 'Double IPA',
-    abv: 9,
-    ibu: '60',
-    price: '$8',
-    pintCount: 86,
-    region: 'local'
-  },
-  {
-    tapped: true,
-    name: 'Pallet Jack',
-    brewery: 'Barley Browns',
-    img: barleyBrown,
-    style: 'IPA',
-    abv: 7,
-    ibu: '63',
-    price: '$7',
-    pintCount: 34,
-    region: 'local'
-  },
-  {
-    tapped: true,
-    name: 'Helles',
-    brewery: 'Buoy Beer Co.',
-    img: buoy,
-    style: 'German Lager',
-    abv: 5.3,
-    ibu: '19',
-    price: '$6',
-    pintCount: 98,
-    region: 'local'
-  },
-  {
-    tapped: true,
-    name: 'Fat Tire',
-    brewery: 'New Belgium',
-    img: fatTire,
-    style: 'Amber Ale',
-    abv: 5.2,
-    ibu: '22',
-    price: '$6',
-    pintCount: 45,
-    region: 'national'
-  },
-  {
-    tapped: true,
-    name: 'NW Red Ale',
-    brewery: 'Buoy Beer Co.',
-    img: buoy,
-    style: 'Red Ale',
-    abv: 6.7,
-    ibu: '75',
-    price: '$7',
-    pintCount: 53,
-    region: 'local'
+class KegList extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      tapped: true,
+      lowPint: false,
+      masterTappedKegList: [
+        {
+          tapped: true,
+          name: 'RPM',
+          brewery: 'Boneyard',
+          img: rpm,
+          style: 'IPA',
+          abv: 6.5,
+          ibu: '50',
+          price: '$7',
+          pintCount: 101,
+          region: 'local',
+          lowPint: false
+        },
+        {
+          tapped: true,
+          name: 'Sculpin',
+          brewery: 'Ballast Point',
+          img: sculpin,
+          style: 'IPA',
+          abv: 7,
+          ibu: '70',
+          price: '$10',
+          pintCount: 23,
+          region: 'national',
+          lowPint: false
+        },
+        {
+          tapped: true,
+          name: 'Furious',
+          brewery: 'Surly Brewing Co',
+          img: furious,
+          style: 'IPA',
+          abv: 6.7,
+          ibu: 'high',
+          price: '$9',
+          pintCount: 64,
+          region: 'national',
+          lowPint: false
+        },
+        {
+          tapped: true,
+          name: 'Super Dog',
+          brewery: 'Lucky Labrador',
+          img: luckyLab,
+          style: 'IPA',
+          abv: 6.5,
+          ibu: '90+',
+          price: '$8',
+          pintCount: 55,
+          region: 'local',
+          lowPint: false
+        },
+        {
+          tapped: true,
+          name: 'Citradelic',
+          brewery: 'New Belgium',
+          img: citradelic,
+          style: 'Tangerine IPA',
+          abv: 6,
+          ibu: '50',
+          price: '$7',
+          pintCount: 1002,
+          region: 'national',
+          lowPint: false
+        },
+        {
+          tapped: true,
+          name: 'Hop Venom',
+          brewery: 'Boneyard',
+          img: hopVenom,
+          style: 'Double IPA',
+          abv: 9,
+          ibu: '60',
+          price: '$8',
+          pintCount: 86,
+          region: 'local',
+          lowPint: false
+        },
+        {
+          tapped: true,
+          name: 'Pallet Jack',
+          brewery: 'Barley Browns',
+          img: barleyBrown,
+          style: 'IPA',
+          abv: 7,
+          ibu: '63',
+          price: '$7',
+          pintCount: 34,
+          region: 'local',
+          lowPint: false
+        },
+        {
+          tapped: true,
+          name: 'Helles',
+          brewery: 'Buoy Beer Co.',
+          img: buoy,
+          style: 'German Lager',
+          abv: 5.3,
+          ibu: '19',
+          price: '$6',
+          pintCount: 98,
+          region: 'local',
+          lowPint: false
+        },
+        {
+          tapped: true,
+          name: 'Fat Tire',
+          brewery: 'New Belgium',
+          img: fatTire,
+          style: 'Amber Ale',
+          abv: 5.2,
+          ibu: '22',
+          price: '$6',
+          pintCount: 45,
+          region: 'national',
+          lowPint: false
+        },
+        {
+          tapped: true,
+          name: 'NW Red Ale',
+          brewery: 'Buoy Beer Co.',
+          img: buoy,
+          style: 'Red Ale',
+          abv: 6.7,
+          ibu: '75',
+          price: '$7',
+          pintCount: 53,
+          region: 'local',
+          lowPint: false
+        }
+      ]
+    };
   }
-];
 
-function KegList() {
-  const kegListStyles ={
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    paddingTop: '20px',
-    width: '97%',
-    paddingBottom: '70px',
-    backgroundColor: 'white',
-    marginBottom: '25px',
-    filter: 'drop-shadow(0 0 0.5rem black)'
-  };
-
-  return(
-    <div style={kegListStyles}>
-      {masterTappedKegList.map((beer, index) =>
-        <Keg tapped={beer.tapped}
-          name={beer.name}
-          brewery={beer.brewery}
-          img={beer.img}
-          style={beer.style}
-          abv={beer.abv}
-          ibu={beer.ibu}
-          price={beer.price}
-          pintCount={beer.pintCount}
-          region={beer.region}
-          key={index}/>
-      )}
-    </div>
-  );
+  render(){
+    return(
+      <div className="kegListStyles">
+        <style jsx>{`
+          .kegListStyles {
+            margin-left: auto;
+            margin-right: auto;
+            padding-top: 20px;
+            width: 97%;
+            padding-bottom: 70px;
+            background-color: white;
+            margin-bottom: 25px;
+            filter: drop-shadow(0 0 0.5rem black);
+          }
+        `}</style>
+      {this.state.masterTappedKegList.map((beer, index) =>
+          <Keg tapped={beer.tapped}
+            name={beer.name}
+            brewery={beer.brewery}
+            img={beer.img}
+            style={beer.style}
+            abv={beer.abv}
+            ibu={beer.ibu}
+            price={beer.price}
+            pintCount={beer.pintCount}
+            region={beer.region}
+            key={index}/>
+        )}
+      </div>
+    );
+  }
 }
 
 export default KegList;
